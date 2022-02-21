29b8324436053e43af0594862de1b0136f9d24ed
Hiroki Noda <kubo39@gmail.com>
2022-02-22 06:47:40 +0900

    intについての注釈

--- make/main.go
+++ make/main.go
@@ -6,6 +6,11 @@ func main() {
 	// https://go.dev/ref/spec#Making_slices_maps_and_channels
 
 	// スライスの長さ・キャパシティはintの範囲
+	// intの定義は32 or 64bitとのこと(アーキテクチャ依存)
+	// ただしptrdiff_tと同じかは不明。
+	// https://go.dev/ref/spec#Numeric_types
+	// capを1<<33にしたところout of memoryになったため、
+	// x86_64ではintは64bitなのでptrdiff_t相当でよさそう？
 	s := make([]int, 10, 10)
 
 	// どうでもいいがPrintlnの出力にコンマは出ない
